845130f759e2594961bcd16c3405cfad2f780360
pankajbhamoriya <38315265+pankajbhamoriya@users.noreply.github.com>
2020-01-06 00:02:42 +0530

    update main

--- MyProject/main.py
+++ MyProject/main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('I update automatically pankaj git next level')
+        self.response.write('I update automatically pankaj git next level 45')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
